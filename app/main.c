7d819bcc4a7b6373f92004d5cf02e2aaf552c940
Samuel Rohrer <srohrer32@gmail.com>
2017-01-09 14:57:50 -0500

    mfcc works on hail, takes ~30 sec for 0.005 sec sample

--- app/main.c
+++ app/main.c
@@ -1,6 +1,5 @@
 /* vim: set sw=2 expandtab tw=80: */
 
-#include <console.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,23 +8,15 @@
 
 // add fft libraries
 #include "fft_lib/fft.h"
-#include "fft_lib/fpmath.h"
+// add mfcc libraries
+#include "libmfcc.h"
 
-#define numel 8
-
-char hello[] = "Hello World!\r\n";
-char bye[] = "Farewell world! I have computed a FFT!!\r\n";
+#define numel 256
 
 void nop() {}
 
 fft_complex_t data[numel];
-
-void printArr() {
-  for (int i = 0; i < numel; i++) {
-    printf("arr[%d], real=%d, imag=%d\n", i, data[i].r, data[i].i);
-  }
-  return;
-}
+double spectrum[numel];
 
 int main() {
 
@@ -33,29 +24,32 @@ int main() {
   int bits = log2(numel);
 
   // fill the array with data
-  /*for (int i = 0; i < numel; i++) {
-    int32_t real = 1;
-    data[i].r = real;
-  }*/
-  data[0].r = 98;
-  data[1].r = 8;
-  data[2].r = 798;
-  data[3].r = 90;
-  data[4].r = 45;
-  data[5].r = 103;
-  data[6].r = 421;
-  data[7].r = 59;
+  for (int i = 0; i < numel; i++) {
+    int32_t real = numel - i;
+    if (i % 2 == 0) {
+      data[i].r = real;
+    } else {
+      data[i].r = -real;
+    }
+  }
 
   printf("data created\n");
-  printArr();
   fft_permutate(data, bits);
-  printf("data permuted \n");
-  printArr();
-
   fft_forward(data, bits);
 
   printf("after FFT\n");
-  printArr();
-  putnstr(bye, sizeof(bye));
+  // make FFT results purely real
+  for (int i = 0; i < numel; i++) {
+    spectrum[i] = data[i].r;
+  }
+  printf("after making spectrum real\n");
+
+  // compute the first 13 coefficients
+  for (int coeff = 0; coeff < 13; coeff++) {
+    double mfcc_result = GetCoefficient(spectrum, 44100, 20, numel, coeff);
+    printf("%i %i\n", coeff, (int)(1000 * mfcc_result));
+  }
+  printf("after mfcc\n");
+
   return 0;
 }
